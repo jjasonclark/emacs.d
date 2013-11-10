59a5e52e965b2b2256b5aa1309e1fffb6689b25c
Jason Clark <jason@jjasonclark.com>
2013-11-09 19:53:58 -0800

    Add setting for other modes

--- configs/init-auto-complete.el
+++ configs/init-auto-complete.el
@@ -1,8 +1,11 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+(add-to-list 'ac-modes 'enh-ruby-mode)
+(add-to-list 'ac-modes 'web-mode)
 (ac-config-default)
 ;; don't show the complete while typing
 (setq ac-auto-start nil)
+(setq ac-ignore-case nil)
 (global-set-key "\M-]" 'auto-complete)
 
 (provide 'init-auto-complete)
