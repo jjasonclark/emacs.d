cd3ff5704e062d6e7e2bdfa0e8271036228c5162
Jason Clark <jason@jjasonclark.com>
2013-11-11 13:14:41 -0800

    start local settings

---
+++ jjasonclark.el
@@ -0,0 +1,8 @@
+;; Display continuous lines
+(setq-default truncate-lines nil)
+
+;; trucate even even when screen is split into multiple windows
+(setq-default truncate-partial-width-windows nil)
+
+;; Turn off menu bar
+(menu-bar-mode -1)
